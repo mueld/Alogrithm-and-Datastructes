f6ca15d8a019d2e8254eb7316248bfdbd685e959
mueld <Dominik.Mueller@bruderer.com>
2023-02-01 07:56:49 +0100

    fill meta data implemented

--- src/Bruderer.Core.Domain/Models/ModelAggregate/ModelComponentScannerV2/Helpers/ModelKeysResolver.cs
+++ src/Bruderer.Core.Domain/Models/ModelAggregate/ModelComponentScannerV2/Helpers/ModelKeysResolver.cs
@@ -65,20 +65,25 @@ namespace Bruderer.Core.Domain.Models.ModelAggregate.ModelComponentScannerV2.Hel
         public override void VisitServiceContainer(PropertyInfo elementProperty, ModelComponentContainer serviceContainer)
         {
             var updatedModelKeys = UpdatedModelKeys(elementProperty, serviceContainer);
-            updatedModelKeys.ServiceName = elementProperty.Name;
             modelkeys.Push(updatedModelKeys);
+            updatedModelKeys.ServiceName = elementProperty.Name;
+            currentKeys = updatedModelKeys;
 
-            
+            FillComponentMetaData();
         }
+
+
         private ModelScanningKeys UpdatedModelKeys(PropertyInfo elementProperty, ModelComponentContainer serviceContainer)
         {
-            var updatedModelKeys = new ModelScanningKeys();
+            var updatedModelKeys = new ModelScanningKeys(currentKeys);
+            updatedModelKeys.ElementName = GetModelName(elementProperty);
             updatedModelKeys.ModelPath = GetModelPath(elementProperty, currentKeys.ModelPath);
             updatedModelKeys.ModelKey = BuildModelKey(updatedModelKeys.ModelPath, GetModelName(elementProperty));
             updatedModelKeys.LocalizationNamespace = UpdateLocalizationNamespace(elementProperty, currentKeys.LocalizationNamespace);
             updatedModelKeys.LocalizationPath = BuildLocalizationPath(currentKeys.LocalizationPath, elementProperty);
             return updatedModelKeys;
         }
+        
         private static string GetModelPath(PropertyInfo elementProperty, string currentPath)
         {
             var modelPath = AttributeResolver.GetModePathAttribute(elementProperty);
@@ -138,6 +143,7 @@ namespace Bruderer.Core.Domain.Models.ModelAggregate.ModelComponentScannerV2.Hel
 
             return newLocalizationNamespace;
         }
+        
         private static string BuildLocalizationPath(string currentPath, PropertyInfo elementProperty, string seperator = StringConstants.Separator)
         {
             if (string.IsNullOrEmpty(currentPath))
@@ -154,15 +160,83 @@ namespace Bruderer.Core.Domain.Models.ModelAggregate.ModelComponentScannerV2.Hel
                 return (currentPath + seperator + elementProperty.Name).ToLower();
             }
         }
-        private static void updateMetaData(ModelScanningKeys keys, PropertyInfo elementProperty, ModelComponentContainer container)
+        
+        private void FillContainerMetaData(PropertyInfo elementProperty, ModelComponentContainer container)
         {
+            container.ModelId = currentKeys.ModelId;
             container.Description.Value = GetLocalizationDescription(elementProperty);
             container.Display.Value = GetLocalizationDisplay(elementProperty);
-            container.ModelLink.Path = keys.ModelPath;
-            container.ModelLink.Key = keys.ModelKey;
-            container.ModelLink.Name = GetModelName(elementProperty);
 
+            container.ModelLink.Path = currentKeys.ModelPath;
+            container.ModelLink.Name = currentKeys.ElementName;
+
+            var localizationPath = BuildLocalizationPath(currentKeys.LocalizationPath, elementProperty);
+            container.Display.Link.Path = localizationPath;
+            container.Display.Link.Name = CoreLocalizationNameConstants.DisplayName;
+            container.Display.Value = GetLocalizationDisplay(elementProperty);
+
+            container.Display.KeyNamespace = currentKeys.LocalizationNamespace;
+            container.Description.Link.Path = localizationPath;
+            container.Description.Link.Name = CoreLocalizationNameConstants.Description;
+            container.Description.Value = GetLocalizationDescription(elementProperty);
+
+            container.ServiceName = currentKeys.ServiceName;
         }
+
+        private void FillVariableMetaData(PropertyInfo elementProperty, ModelVariableBase modelVariableBase)
+        {
+            modelVariableBase.ModelId = currentKeys.ModelId;
+            modelVariableBase.ModelLink.Path = currentKeys.ModelPath;
+            modelVariableBase.ModelLink.Name = currentKeys.ElementName;
+
+            modelVariableBase.Display.Link.Path = BuildLocalizationPath(currentKeys.LocalizationPath, elementProperty);
+            modelVariableBase.Display.Link.Name = CoreLocalizationNameConstants.DisplayName;
+            modelVariableBase.Display.KeyNamespace = currentKeys.LocalizationNamespace;
+            modelVariableBase.Display.Value = GetLocalizationDisplay(elementProperty);
+            modelVariableBase.Description.Link.Path = BuildLocalizationPath(currentKeys.LocalizationPath, elementProperty);
+            modelVariableBase.Description.Link.Name = CoreLocalizationNameConstants.Description;
+            modelVariableBase.Description.KeyNamespace = currentKeys.LocalizationNamespace;
+            modelVariableBase.Description.Value = GetLocalizationDescription(elementProperty);
+
+            modelVariableBase.ServiceName = currentKeys.ServiceName;
+        }
+
+        private void FillEnumerableModelContainerMetaData(PropertyInfo elementProperty, IModelComponentContainerCollection collectionModelContainer)
+        {
+            collectionModelContainer.ModelLink.Path = currentKeys.ModelPath;
+            collectionModelContainer.ModelLink.Name = currentKeys.ElementName;
+
+            collectionModelContainer.Display.Link.Path = BuildLocalizationPath(currentKeys.LocalizationPath, elementProperty);
+            collectionModelContainer.Display.Link.Name = CoreLocalizationNameConstants.DisplayName;
+            collectionModelContainer.Display.KeyNamespace = currentKeys.LocalizationNamespace;
+            collectionModelContainer.Display.Value = GetLocalizationDisplay(elementProperty);
+            collectionModelContainer.Description.Link.Path = BuildLocalizationPath(currentKeys.LocalizationPath, elementProperty);
+            collectionModelContainer.Description.Link.Name = CoreLocalizationNameConstants.Description;
+            collectionModelContainer.Description.KeyNamespace = currentKeys.LocalizationNamespace;
+            collectionModelContainer.Description.Value = GetLocalizationDescription(elementProperty);
+
+            collectionModelContainer.ServiceName = currentKeys.ServiceName;
+        }
+
+        private void FillRPCMetaData(PropertyInfo elementProperty,ModelRPCBase modelRPCBase)
+        {
+            modelRPCBase.ModelId = currentKeys.ModelId;
+
+            modelRPCBase.Display.Link.Path = currentKeys.LocalizationPath;
+            modelRPCBase.Display.Link.Name = CoreLocalizationNameConstants.DisplayName;
+            modelRPCBase.Display.KeyNamespace = currentKeys.LocalizationNamespace;
+            modelRPCBase.Display.Value = GetLocalizationDisplay(elementProperty);
+            modelRPCBase.Description.Link.Path = currentKeys.LocalizationPath;
+            modelRPCBase.Description.Link.Name = CoreLocalizationNameConstants.Description;
+            modelRPCBase.Description.KeyNamespace = currentKeys.LocalizationNamespace;
+            modelRPCBase.Description.Value = GetLocalizationDescription(elementProperty);
+
+            modelRPCBase.ModelLink.Path = currentKeys.ModelPath;
+            modelRPCBase.ModelLink.Name = currentKeys.ElementName;
+
+            modelRPCBase.ServiceName = currentKeys.ServiceName;
+        }
+
         private static string GetLocalizationDescription(PropertyInfo elementProperty, bool useTypeAttributes = false)
         {
             var elementName = string.Empty; //elementProperty.Name;
@@ -179,7 +253,8 @@ namespace Bruderer.Core.Domain.Models.ModelAggregate.ModelComponentScannerV2.Hel
 
             return elementName;
                 }
-        protected static string GetLocalizationDisplay(PropertyInfo elementProperty, bool useTypeAttributes = false)
+       
+        private  static string GetLocalizationDisplay(PropertyInfo elementProperty, bool useTypeAttributes = false)
         {
             var elementName = string.Empty; //elementProperty.Name;
             string attributeDisplayName = string.Empty;
@@ -195,11 +270,24 @@ namespace Bruderer.Core.Domain.Models.ModelAggregate.ModelComponentScannerV2.Hel
 
             return elementName;
         }
+        
     }
 
     internal class ModelScanningKeys
     {
+
+        public ModelScanningKeys() { }
+        public ModelScanningKeys(ModelScanningKeys keys)
+        {
+            ModelId = keys.ModelId;
+            ModelPath = keys.ModelPath;
+            LocalizationPath = keys.LocalizationPath;
+            ServiceName = keys.ServiceName;
+            LocalizationNamespace = keys.LocalizationNamespace;
+        }
+
         public Guid ModelId;
+        public string ElementName = string.Empty;
         public string ModelPath = string.Empty;
         public string ModelKey = string.Empty;
         public string LocalizationPath = string.Empty;
